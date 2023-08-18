1a6d06304b3ee4f12581efba881ad75127e3cd68
themightyfrogge <themightyfrogge@gmail.com>
2023-08-18 17:25:58 +0300

    file format

--- src/main/java/kiul/tierblock/listeners/MiningListener.java
+++ src/main/java/kiul/tierblock/listeners/MiningListener.java
@@ -24,71 +24,81 @@ public class MiningListener implements Listener {
     @EventHandler
     public void blockBreak(BlockBreakEvent event) {
         MineableType type = MineableType.fromMaterial(event.getBlock().getType());
-		
-		if(type == null) return;
-		if(event.getBlock().hasMetadata("pp")) return; // pp as in player-placed, no other meaning.... trust me
-		User user = UserManager.getInstance().getUser(event.getPlayer());
-		
-		event.setDropItems(false);
-		
-		if(user.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
-		
-		if(type.isNether && !user.getStats().getBoolean("mining.nether.unlocked")) {
-			user.sendActionBar("&cYou need to &emax mining &cto unlock nether content!");
-			return;
-		}
-		
-		int globalReq = Main.getGlobalLevelRequirement("mining." + type.label);
 
-        if(user.getGlobalLevel() < globalReq) {
+        if (type == null)
+            return;
+        
+        if (event.getBlock().hasMetadata("pp"))
+            return; // pp as in player-placed, no other meaning.... trust me
+
+        User user = UserManager.getInstance().getUser(event.getPlayer());
+
+        event.setDropItems(false);
+
+        if (user.getPlayer().getGameMode() != GameMode.SURVIVAL)
+            return;
+
+        if (type.isNether && !user.getStats().getBoolean("mining.nether.unlocked")) {
+            user.sendActionBar("&cYou need to &emax mining &cto unlock nether content!");
+            return;
+        }
+
+        int globalReq = Main.getGlobalLevelRequirement("mining." + type.label);
+
+        if (user.getGlobalLevel() < globalReq) {
             user.sendActionBar("&cYou need &eisland level " + globalReq + " &cto collect this!");
             return;
         }
-		
-        if(user.getLevel(SkillType.MINING, type.isNether) < type.levelRequirement) {
-            // show progress, if the block is unlockable in the next level (In other words: block level = user level + 1)
+
+        if (user.getLevel(SkillType.MINING, type.isNether) < type.levelRequirement) {
+            // show progress, if the block is unlockable in the next level (In other words:
+            // block level = user level + 1)
             // if not tell them the level requirement:
             boolean progress = (type.levelRequirement - 1) == user.getLevel(SkillType.MINING, type.isNether);
-            if(progress) {
-				MineableType previousType = MineableType.values()[type.ordinal() - 1];
+            if (progress) {
+                MineableType previousType = MineableType.values()[type.ordinal() - 1];
                 user.sendActionBar(
                     String.format(
-                        "&cYou need to break &e%s &cmore &e%s&c to collect this!", 
-                        (int)(previousType.levelUp - user.getExperience(SkillType.MINING, type.isNether)),
-                        previousType.formatName()
-                    )
-                );
+                        "&cYou need to break &e%s &cmore &e%s&c to collect this!",
+                        (int) (previousType.levelUp - user.getExperience(SkillType.MINING, type.isNether)),
+                        previousType.formatName())
+                    );
                 return;
             }
-			
-			user.sendActionBar("&cYou need to have &emining level " + type.levelRequirement + " &cto farm this!");
+
+            user.sendActionBar("&cYou need to have &emining level " + type.levelRequirement + " &cto farm this!");
             return;
         }
 
-		event.setDropItems(true);
+        event.setDropItems(true);
 
         double xpReward = 0.0;
 
-        if(type.levelRequirement == user.getLevel(SkillType.MINING, type.isNether)
-			|| (type.levelRequirement == 0 && user.getLevel(SkillType.MINING, type.isNether) == 1)) {
-			xpReward = user.addExperience(SkillType.MINING, 1.0, type.isNether);
+        if (type.levelRequirement == user.getLevel(SkillType.MINING, type.isNether)
+            || (type.levelRequirement == 0 && user.getLevel(SkillType.MINING, type.isNether) == 1)) {
+            xpReward = user.addExperience(SkillType.MINING, 1.0, type.isNether);
         }
-		
+
         user.sendActionBar(
-            new StringBuilder(String.format(
-                "&eIsland: &2+&a%sxp " + (user.getBoosterMultiplier() > 1.0 ? "(x" + (int)user.getBoosterMultiplier() + " booster) " : ""),
-                Main.DECIMAL_FORMAT.format(user.addGlobalExperience(type.xpReward))
-            )).append(
-                (xpReward <= 0.0 ? "" : String.format("&8| &eMining: &2+&a%sxp &8(&b%s&8)", Main.DECIMAL_FORMAT.format(xpReward), type.formatName()))
-            ).toString()
-        );
+            new StringBuilder(
+                String.format(
+                 "&eIsland: &2+&a%sxp " + (user.getBoosterMultiplier() > 1.0
+                    ? "(x" + (int) user.getBoosterMultiplier() + " booster) " : ""),
+                    Main.DECIMAL_FORMAT.format(user.addGlobalExperience(type.xpReward)))
+                )
+            .append((xpReward <= 0.0 ? ""
+                : String.format("&8| &eMining: &2+&a%sxp &8(&b%s&8)",
+                Main.DECIMAL_FORMAT.format(xpReward), type.formatName()))).toString()
+            );
     }
 
     @EventHandler
     public void blockPlace(BlockPlaceEvent event) {
         MineableType type = MineableType.fromMaterial(event.getBlock().getType());
-        
-        if(type == null) return;
+
+        if (type == null)
+            return;
+            
         // block flagged, at least, until a restart is issued. (metadata not persistent)
         event.getBlock().setMetadata("pp", new FixedMetadataValue(Main.getInstance(), "f"));
     }
